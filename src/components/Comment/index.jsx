57d2342af0b06e1d1fc740152ff6c152937fe34e
Gabriel Sousa <79923219+Gabriel-Sousa@users.noreply.github.com>
2022-07-11 20:45:51 -0300

    Initial commit

---
+++ src/components/Comment/index.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Avatar } from "../Avatar";
+import { Trash, ThumbsUp } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => state + 1);
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src={"https://github.com/Gabriel-Sousa.png"} />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title="11 de Maio ás 08:13h" dateTime="2022-05-11 08:13:30">
+                Públicado há 1h
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} tittle="Deletar Comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
